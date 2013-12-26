e3c48bb4370f7bb470fc06bdda1f4ab3e8518d35
Or David <ordavidil@gmail.com>
2013-12-26 14:23:45 +0200

    added Family/Image/obs  classes to Models.py

--- src/pages/models.py
+++ src/pages/models.py
@@ -2,8 +2,11 @@
 from django.db import models
 
 # Create your models here.
-class Specie(models.Model):
+class Species(models.Model):
 
+    # huji_species_id - מזהה מין בטבלת "הגר" (INT)
+    huji_species_id = models.IntegerField()
+    
     # seintific_name - שם מדעי (VARCHAR)
     seintific_name = models.CharField(max_length=200)
 
@@ -70,24 +73,88 @@ class Specie(models.Model):
 # family_id - מזהה משפחה (INT) - מפתח זר
 
 
+class ObsservationTypes(object):
+    WI = 4 # escaped to the wild
+    LI = 3 # literature site
+    EP = 2 # episode site
+    A65 = 1 # after 1965
+    B65 = 0 # before 1965
+
+    choices = (
+               (B65, 'sites from which data was collected prior to 1965, and the species is now extinct.'),
+               (A65, 'sites from which data was collected after 1965.'),
+               (EP, 'episodic; sites where the plant was observed but no permanent population remains.'),
+               (LI, 'uncertain site, based only on reports from the literature but has not been currently verified in the field.'),
+               (WI, 'sites where the plant is cultivated (originating from a cultivated population) or introduced. See, for example, Iris pseudacorus.'),
+              )
+
+
+class IUCNLevel(object):
+    EX = 6
+    EW = 5
+    CR = 4
+    EN = 3
+    VU = 2
+    NT = 1
+    LC = 0
+
+    choices = (
+               (LC, 'Least concern'),
+               (EX, 'Extinct'),
+               (EW, 'Extinct in the wild'),
+               (CR, 'Critically endangered'),
+               (EN, 'Endangered'),
+               (VU, 'Vulnerable'),
+               (NT, 'Near threatened'),
+              )
 
 
 
+class Families(models.Model):
+    # he_name - שם עברי (VARCHAR)
+    family_he_name = models.CharField(max_length=100, db_index=True)
+    # en_name - שם אנגלי (VARCHAR)
+    family_en_name = models.CharField(max_length=100, db_index=True)
+    
+    def __unicode__(self):
+        return self.family_en_name
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Images(models.Model):
+    
+    # species_id - מפתח זר
+    species_id = models.ForeignKey(Species)
+    
+    # url - כתובת התמונה (URL)
+    url = models.URLField()
+    
+    # photographer - שם הצלם - (VARCHAR)
+    photographer = models.CharField(max_length=50, db_index=True)
+    
+    # ordinal - סדר התמונות
+    ordinal = models.IntegerField()
+      
+    def __unicode(self):
+        return self.url
+      
+      
+class Obs(models.Model):
+    
+    # species_id - מפתח זר
+    species_id = models.ForeignKey(Species)
+    
+    # obs_x - קו אורך (FLOAT)
+    obs_lng = models.FloatField()
+    
+    # obs_y - קו רוחב (FLOAT)
+    obs_lat = models.FloatField()
+    
+    # obs_date - תאריך תצפית (DATE) null
+    obs_date = models.DateField(null=True, blank=True)
+    
+    # aggregation_num - מספר תצפיות (INT)
+    aggregation_num = models.IntegerField()
+    
+    # obs_type_id - סוג תצפית (INT) - מפתח זר
+    obs_type = models.IntegerField() #int replaced by ENUM - check Udi's example
 
 
--- src/redSpecies/settings.py
+++ src/redSpecies/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'index',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.abspath(os.path.join(BASE_DIR, "redSpecies","static")),
+                   )
