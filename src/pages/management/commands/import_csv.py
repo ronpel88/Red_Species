dbbf2100439e56529aca991dea838d004a997edd
Ron <ronpel88@gmail.com>
2013-12-26 12:05:18 +0200

    Completed to import book csv into species model.

---
+++ src/pages/management/commands/import_csv.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from django.core.management.base import BaseCommand
+import csv
+from pages.models import Species
+
+
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Starting to import data from csv to model")
+        with open("csv/RedSpeciesDataTable.csv") as f:
+            r = csv.DictReader(f)
+            for d in r:
+                species = Species(seintific_name=d['Scientific_Name'],
+                            en_name=d['English_Name'],
+                            he_name=d['Scientific_Name'],
+                            author=d['Scientific_Name'],
+                            protection_status=bool(d['Protection_by_Law_State']),
+                            eco_system_text=d['Ecosystem'],
+                            chorotype_text=d['Chorotype'],
+                            conservation_site=d['Main_conservation_sites'],
+                            rarity=int(d['Vulnerability']),
+                            red_number=d['Red_index'],
+                            iucn_category=d['IUCN_Category'],
+                            vulnerability=int(d['Vulnerability']),
+                            attractiveness=int(d['Attractiveness']),
+                            endemism=int(d['Endemism']),
+                            peripherality=bool(d['Peripherality']),
+                            disjunctiveness=d['Disjunctiveness'],
+                            number_of_districts=int(d['Number_of_Districts']),
+                            percentage_of_protected_sites=int(d['Percentage_of_protected_sites']),
+                            plant_description=d['Plant_Description'],
+                            uses=d['Uses'],
+                            distribution_in_israel=d['Distribution_in_Israel'],
+                            habitat=d['Habitat'],
+                            global_distribution=d['Global_Distribution'],
+                            systematics_and_biogeography=d['Systematics_and_Biogeography'],
+                            nature_conservation=d['Nature_Conservation'],
+                            conservation_recomendations=d['Recommendations_for_Management_and_Conservation'],
+                            discussion_and_conclusions=d['Discussion_and_conclusions'],
+                            literature=d['Literature'])
+                species.save()
+
+
+
+
+        self.stdout.write("Done!")
+        # l = Species.objects.all()
+        # for x in l:
+        #     print x
--- src/pages/models.py
+++ src/pages/models.py
@@ -2,42 +2,42 @@
 from django.db import models
 
 # Create your models here.
-class Specie(models.Model):
+class Species(models.Model):
 
     # seintific_name - שם מדעי (VARCHAR)
-    seintific_name = models.CharField(max_length=200)
+    seintific_name = models.CharField(max_length=200, unique=True)
 
-    # en_name - שם אנגלי (VARCHAR)
+    # # en_name - שם אנגלי (VARCHAR)
     en_name = models.CharField(max_length=200)
 
-    # he_name - שם עברי (VARCHAR)
+    # # he_name - שם עברי (VARCHAR)
     he_name = models.CharField(max_length=200)
 
-    # author - מחבר (VARCHAR)
+    # # author - מחבר (VARCHAR)
     author = models.CharField(max_length=200)
 
-    # protection_status - האם מוגן (BOOLEAN)
+    # # protection_status - האם מוגן (BOOLEAN)
     protection_status = models.BooleanField()
 
-    # eco_system - מערכת אקולוגית (VARCHAR)
+    # # eco_system - מערכת אקולוגית (VARCHAR)
     eco_system_text = models.CharField(max_length=200)
 
-    # chorotype - כורוטיפ (VARCHAR)
+    # # chorotype - כורוטיפ (VARCHAR)
     chorotype_text = models.CharField(max_length=200)
 
-    # conservation_site - אתר מרכזי לשימור (VARCHAR)
+    # # conservation_site - אתר מרכזי לשימור (VARCHAR)
     conservation_site = models.CharField(max_length=200)
 
-    # rarity - נדירות (INT)
+    # # rarity - נדירות (INT)
     rarity = models.SmallIntegerField()
 
-    # red_number - מספר אדום (INT)
-    red_number = models.SmallIntegerField()
+    # # red_number - מספר אדום (INT)
+    red_number = models.CharField(max_length=200)
 
-    # iucn_category - קטגוריה לפי איגוד עולמי (CHAR)
+    # # iucn_category - קטגוריה לפי איגוד עולמי (CHAR)
     iucn_category = models.CharField(max_length=200)
 
-    # vulnerability - פגיעות (INT)
+    # # vulnerability - פגיעות (INT)
     vulnerability = models.SmallIntegerField()
 
     # attractiveness - אטרקטיביות (INT)
@@ -50,27 +50,49 @@ class Specie(models.Model):
     peripherality = models.SmallIntegerField()
 
     # disjunctiveness -  צמידות (INT)
-    disjunctiveness = models.SmallIntegerField()
+    disjunctiveness = models.CharField(max_length=200)
 
     # number_of_districts - מספר גלילות (INT)
-    number_of_districts = models.SmallIntegerField()
+    number_of_districts = models.IntegerField()
 
     # percentage_of_protected_sites - אחוז האתרים המוגנים (INT)
+    percentage_of_protected_sites = models.IntegerField()
+
     # plant_description - תיאור הצמח (TEXT)
+    plant_description = models.TextField()
+
     # uses - שימושים (TEXT)
+    uses = models.TextField()
+
     # distribution_in_israel - תפוצה בארץ (TEXT)
+    distribution_in_israel = models.TextField()
+
     # habitat - בית גידול (TEXT)
+    habitat = models.TextField()
+
     # global_distribution - תפוצה בעולם (TEXT)
+    global_distribution = models.TextField()
+
     # systematics_and_biogeography - סיסטמטיקה וביוגיאוגרפיה (TEXT)
+    systematics_and_biogeography = models.TextField()
+
     # nature_conservation - שמירת טבעי (TEXT)
+    nature_conservation = models.TextField()
+
     # conservation_recomendations - המלצות לממשק ושימור (TEXT)
+    conservation_recomendations = models.TextField()
+
     # discussion_and_conclusions - סיכום ומסקנות (TEXT)
+    discussion_and_conclusions = models.TextField()
+
     # literature - ספרות (TEXT)
+    literature = models.TextField()
 
-# family_id - מזהה משפחה (INT) - מפתח זר
+    # family_id - מזהה משפחה (INT) - מפתח זר
+    # family_id = models.ForeignKey()
 
-
-
+    def __unicode__(self):
+        return self.seintific_name
 
 
 
